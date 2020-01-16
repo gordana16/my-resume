9a5bf4d30c51128f1e75528b1d0ded35b49a6fc1
Gordana Nikolic <gordana.nikolic6@gmail.com>
2020-01-16 22:10:01 +0000

    build the .env using a script before running build script for deployment

---
+++ script/create-env.js
@@ -0,0 +1,6 @@
+const fs = require("fs");
+
+fs.writeFileSync(
+  "./.env",
+  `NAME=${process.env.NAME}\nLABEL=${process.env.LABEL}\nEMAIL=${process.env.EMAIL}\nPHONE=${process.env.PHONE}\nSUMMARY=${process.env.SUMMARY}\nADDRESS=${process.env.ADDRESS}\nPOSTAL_CODE=${process.env.POSTAL_CODE}\nCITY=${process.env.CITY}\nCOUNTRY_CODE=${process.env.COUNTRY_CODE}\nREGION=${process.env.REGION}\nPROFILES=${process.env.PROFILES}\nWORK=${process.env.WORK}\nSKILLS=${process.env.SKILLS}\nEDUCATION=${process.env.EDUCATION}\nCERTIFICATE=${process.env.CERTIFICATES}\nLANGUAGES=${process.env.LANGUAGES}\n`
+);
